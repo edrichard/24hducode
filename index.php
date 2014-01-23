455dd68e57de9fbfdcca343b519a7722bf585f45
Lefebvre76 <lefebvre.loic.76@gmail.com>
2014-01-23 13:18:09 +0100

    test password

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<?php echo "Welcome in 24H du Code 2014 !! Youpi"; ?>
+	<?php echo "Welcome in 24H du Code 2014 !! Youpi !!!"; ?>
 </body>
 </html>
