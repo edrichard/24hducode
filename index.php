63c50bef85f1d67dd42bd28722ed14db5deda1a5
Tristan Papillon <papillon.tristan@gmail.com>
2014-01-23 22:20:24 +0100

    Quand il pète il troue son slip

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<?php echo "Welcome in 24H du Code 2014 Les't go 25 et 26 janvier!! eddy richard"; ?>
+	<?php
+    echo '<p>Welcome in 24H du Code 2014 Let\'s go 25 et 26 janvier!! eddy richard</p>';
+    echo '<p>Et quand il pète, il troue son slip!</p>';
+    ?>
+    
 </body>
 </html>
