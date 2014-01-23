a0d34c0b7a2545a5ca87ce22e2b36e20e5b96475
RICHARD Eddy <richard.eddy@live.fr>
2014-01-23 21:24:23 +0100

    mise a jour

--- index.php
+++ index.php
@@ -5,10 +5,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<<<<<<< HEAD
 	<?php echo "Welcome in 24H du Code 2014 Les't go 25 et 26 janvier!!"; ?>
-=======
-	<?php echo "Welcome in 24H du Code 2014 Les't go 25 et 26 janvier!! ajout test loic"; ?>
->>>>>>> afc954fd02cb15a99830f6fcc91fc2cef830dd29
 </body>
 </html>
