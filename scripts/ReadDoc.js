4dbc243eb1955c67c177249c93205ceb77557700
Tristan Papillon <papillon.tristan@gmail.com>
2014-01-25 23:49:52 +0100

    Chargement d'un fichier GTFS OK

--- scripts/ReadDoc.js
+++ scripts/ReadDoc.js
@@ -1,51 +1,50 @@
 $(function() {
 
-	loadInfos("GTFS/stop_times.txt").done(function(data){
-		console.log(typeof data);
-	});
-	
-	$("#infos_passage").append(dataTime.toSource());
+    loadInfos("GTFS/routes_additionals.txt").done(function(data){
+        data = parseGTFS(data);
+        // console.log(data);
+        // console.log(data[0]); // headers
+        // console.log(data[1]); // premiere occurence
+        // console.log(data[1]['route_desc']); // route_desc de la premiere occurence
+    });
 
 });
+
+
+function parseGTFS(data) {
+    var result = new Array(); // My list of table
+    var listItem = new Array(); // My item list
+    var list; // My list of details
+    
+    // Foreach lign...
+    $.each(data.split('\n'), function(i, d) {
+        
+        // If it's my first lign with my list of id
+        if (i == 0)
+        {
+            $.each(d.split(','), function(j, item) {
+                listItem.push(item);
+            });
+
+        }
+        else
+        {
+            list = {};
+            $.each(d.split('","'), function(j, detail) {
+
+                var col = listItem[j]; 
+                list[col] = detail;
+
+            });
+            result.push(list);
+        }
+    });
+    return result;
+}
 
 
 // Load file
 function loadInfos(file)
 {
-    var infos = $.get(file, function(data) {
-
-
-        var result = new Array(); // My list of table
-        var listItem = new Array(); // My item list
-        var list; // My list of details
-        
-        // Foreach lign...
-        $.each(data.split('\n'), function(i, d) {
-            
-            // If it's my first lign with my list of id
-            if (i == 0)
-            {
-                $.each(d.split(','), function(j, item) {
-                    listItem.push(item);
-					
-                });
-
-            }
-            else
-            {
-                list = {};
-                $.each(d.split(','), function(j, detail) {
-                    //list.push(detail);
-                    var col = listItem[j]; 
-                    list[col] = detail;
-					
-
-                });
-                result.push(list);
-            }
-        });
-        return result;
-    });
-
-    return infos;
+    return $.get(file);
 }
