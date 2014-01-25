8e3a550c137cb0fd4e0b623845b7fde0b40d63fb
Lefebvre76 <lefebvre.loic.76@gmail.com>
2014-01-25 15:49:27 +0100

    Fonction pour parser les fichiers txt

---
+++ scripts/ReadDoc.js
@@ -0,0 +1,41 @@
+// Load file
+function LoadInfos(file)
+{
+    var infos = $.get(file, function(data) {
+
+        var result = new Array(); // My list of table
+        var listItem = new Array(); // My item list
+        var list; // My list of details
+        
+        // Foreach lign...
+        $.each(data.split('\n'), function(i, d) {
+            
+            // If it's my first lign with my list of id
+            if (i == 0)
+            {
+                $.each(d.split(','), function(j, item) {
+                    listItem.push(item);
+                });
+
+            }
+            else
+            {
+                list = {};
+                $.each(d.split(','), function(j, detail) {
+                    //list.push(detail);
+                    var col = listItem[j]; 
+                    list[col] = detail;
+
+                });
+                result.push(list);
+            }
+        });
+        return result;
+    });
+    
+    return infos;
+}
+
+function getFile(file) {
+    return LoadInfos('GTFS/routes_additionals.txt');
+}
