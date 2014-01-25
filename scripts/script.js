1a5dafa8285ccac4a232552a5abd250ecd92c9f6
RICHARD Eddy <richard.eddy@live.fr>
2014-01-25 12:49:04 +0100

    Chargement adresse flux RSS

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $(document).ready(  function()
 	{
 		$.ajax( {
 	        type: "GET",
-	        url: "Fichier.xml",
+	        url: "http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/feed",
 	        dataType: "xml",
 	        success: function(xml) { }
 	    });
