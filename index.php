fde786849a7e0e44b5d92ba6d340d3e14e6a80bf
Tristan Papillon <papillon.tristan@gmail.com>
2014-01-25 11:54:42 +0100

    Ajout manifest pour Firefox OS

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+    <meta charset="UTF-8">
+    <title>Insert title here</title>
 </head>
 <body>
-<<<<<<< HEAD
-	<?php
-    echo '<p>Welcome in 24H du Code 2014 Let\'s go 25 et 26 janvier!! eddy richard</p>';
-    echo '<p>Et quand il pète, il troue son slip!</p>';
-    ?>
-    
-=======
-	<?php echo "Welcome in 24H du Code 2014 !! Youpi c'est demain !!"; ?>
->>>>>>> alex
+    <?php echo 'hello world'; ?>
 </body>
 </html>
