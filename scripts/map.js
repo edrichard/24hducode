7e382398ef5e5cd2231c62dbeffaaecbdc0b8fd9
Tristan Papillon <papillon.tristan@gmail.com>
2014-01-26 05:09:42 +0100

    Zoom map

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,7 @@
 $(function() {
     initialize();
     showArreteMarker();
+    setZoom(16);
 });
 
 var map;
@@ -23,41 +24,39 @@ function showArreteMarker() {
     });
 }
 
+function setZoom(zoom) {
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+        map.setZoom(zoom);
+        map.setCenter(marker.getPosition());
+    });
+}
+
 function initialize() {
     var mapOptions = {
-        zoom : 10
+        zoom : 16
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var pos = new google.maps.LatLng(48.103648, -1.672379);
-
-    var infowindow = new google.maps.InfoWindow({
-        map : map,
-        position : pos,
-        content : 'Vous êtes ici.'
-    });
-    
-    map.setCenter(pos);
-
     // Try HTML5 geolocation
-    /*if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function(position) {
-     var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(48.103648, -1.672379);
+            //var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-     var infowindow = new google.maps.InfoWindow({
-     map : map,
-     position : pos,
-     content : 'Vous êtes ici.'
-     });
+            var infowindow = new google.maps.InfoWindow({
+                map : map,
+                position : pos,
+                content : 'Vous êtes ici.'
+            });
 
-     map.setCenter(pos);
-     }, function() {
-     handleNoGeolocation(true);
-     });
-     } else {
-     // Browser doesn't support Geolocation
-     handleNoGeolocation(false);
-     }*/
+            map.setCenter(pos);
+        }, function() {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
 }
 
 function handleNoGeolocation(errorFlag) {
