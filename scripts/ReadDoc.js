626526334a21b1f6dcbd5a106219b957dbb19570
Tristan Papillon <papillon.tristan@gmail.com>
2014-01-26 04:30:10 +0100

    Merge

--- scripts/ReadDoc.js
+++ scripts/ReadDoc.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-$(function(){
-	LoadInfos("GTFS/stops.txt").done(function(data){
-		//console.log(typeof data);
-		
-		var obj = jQuery.parseJSON( data );
-		console.log(obj);
-		
-		
-});
-
-// Load file
-function LoadInfos(file)
-{
-    var infos = $.get(file, function(data) {
-
-        var result = new Array(); // My list of table
-        var listItem = new Array(); // My item list
-        var list; // My list of details
-        
-        // Foreach line...
-        $.each(data.split('\n'), function(i, d) {
-            
-            // If it's my first line with my list of id
-            if (i == 0) {
-                $.each(d.split(','), function(j, item) {
-                    listItem.push(item);
-                });
-            } else {
-                list = {};
-                $.each(d.split(','), function(j, detail) {
-                    //list.push(detail);
-                    var col = listItem[j]; 
-                    list[col] = detail;
-                });
-                result.push(list);
-            }
-        });
-        return result;
-    });
-    return infos;
-}
-=======
-$(function() {
-
-    loadInfos("GTFS/stops.txt").done(function(data){
-        data = parseGTFS(data);
-    });
-
-});
-
-
 function parseGTFS(data) {
     var result = new Array(); // My list of table
     var listItem = new Array(); // My item list
@@ -87,4 +35,3 @@ function loadInfos(file)
 {
     return $.get(file);
 }
->>>>>>> 72807bee2c8ac03488b4cedeecdb3238991de3de
