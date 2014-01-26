ea3e79d0d239fa2fdc5d45845092f7690ae973f1
Houlle <alexandre.h72@gmail.com>
2014-01-26 07:12:34 +0100

    InfoWindow_maps

--- scripts/map.js
+++ scripts/map.js
@@ -4,24 +4,57 @@ $(function() {
 });
 
 var map;
+var infowindow;
 
 function showArreteMarker() {
     loadInfos("GTFS/stops.txt").done(function(data) {
         data = parseGTFS(data);
 
         $.each(data, function(index, value) {
-            console.log(value['stop_lat']);
-            console.log(value['stop_lon']);
+           // console.log(value['stop_lat']);
+           
             var myLatlng = new google.maps.LatLng(value['stop_lat'], value['stop_lon']);
-            new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position : myLatlng,
                 map : map,
-                title : 'Hello World!'
+                title : value['stop_name']
             });
+			
 
-        });
+			//var nom = "Le nom de l'arret est :"+value['stop_name']+"!!!!";
+	console.log(value['stop_id']);
+			var contentString = '<div id="content">'+
+				  '<div id="siteNotice">'+
+				  '</div>'+
+				  '<h1 id="firstHeading" class="firstHeading">Nom de l\'arret</h1>'+
+				  '<div id="bodyContent" onclick="loadTrips('+value['stop_id']+')">'+
+				  'pfhqpqpirubgfmqufbgpqudvn^qoruihgùsivhnqrihvùgsirhvqmruhglqduhvlgdfubvqmrunhvsfdvnqmrqfuvbqruvbqruvbqsmruvmr<hmsdigqmruhgfigq^rhgiifghsroh'+
+				  '</div>'+
+				  '</div>';
+
+	   
+	   
+
+	  
+		 infowindow = new google.maps.InfoWindow({
+			  content: contentString
+		  });
+		  
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+			
+
+			
     });
+});
 }
+
+function modif_content(data){
+
+			infowindow.setContent(data);
+
+}	
 
 function initialize() {
     var mapOptions = {
@@ -36,7 +69,11 @@ function initialize() {
         position : pos,
         content : 'Vous êtes ici.'
     });
-    
+	
+
+
+	
+	
     map.setCenter(pos);
 
     // Try HTML5 geolocation
@@ -58,6 +95,32 @@ function initialize() {
      // Browser doesn't support Geolocation
      handleNoGeolocation(false);
      }*/
+			 // var dateNow = new Date().getTime();
+			
+			// Récupération de l'heure du départ du bus de l'arrêt
+			 // var heureDepBus = "05:46:00";
+			
+			// Récupération de la date pour  la comparaison
+			 // var jourSemDep = new Date().getDate();
+			 // var moisDateDep = new Date().getMonth();
+			 // var nomJourDateDep = new Date().getDay();
+			 // var anneeDateDep = new Date().getFullYear();
+			
+			// var dateDep = jourSemDep+','+moisDateDep+' '+nomJourDateDep+' '+anneeDateDep+' '+heureDepBus;
+			
+			// Parsage en timestamp
+			// / var dateDepTM = new Date(anneeDateDep, moisDateDep, jourSemDep ).getTime();
+			// var dateDepTm = var Date(dateDep).getTime();
+			
+			// console.log(dateDepTM);
+	
+		// var mapBodyContent = document.getElementById('#bodyContent');
+		// console.log(mapBodyContent);
+		 // google.maps.event.addListener(mapBodyContent, 'click', function(){
+			// infowindow.setContent("tralalala");
+         // });
+	
+	
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -76,3 +139,32 @@ function handleNoGeolocation(errorFlag) {
     var infowindow = new google.maps.InfoWindow(options);
     map.setCenter(options.position);
 }
+
+function loadTrips(stop_id)
+{
+ loadInfos("GTFS/stop_times.txt").done(function(data) {
+        data = parseGTFS(data);
+
+        $.each(data, function(index, value) {
+
+		
+			if(value['stop_id'] == stop_id)
+			{
+				var mesDatas = "<p>Prochains départs : "+value['arrival_time']+"</p>";
+				console.log(value['arrival_time']);
+				modif_content(mesDatas);
+			}
+			
+			
+           
+
+        });
+    });
+
+}
+
+function compareDate(date1, date2){
+	
+}
+
+
